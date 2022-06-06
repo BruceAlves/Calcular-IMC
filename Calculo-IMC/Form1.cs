9ea539f7cf058892fc4309d2c3bc1fbc6dd24775
BruceAlves <brucewallace144@gmail.com>
2022-06-06 05:52:05 -0300

    Calcular IMC

---
+++ Calculo-IMC/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double peso = Convert.ToDouble(txtPeso.Text);
+            double altura = Convert.ToDouble(txtAltura.Text);
+            double imc = peso / (altura * altura);
+
+            MessageBox.Show($"Seu IMC é {imc.ToString("F")}","Resultado IMC", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtPeso.Text = string.Empty;
+            txtAltura.Text = string.Empty;
+            
+        }
+    }
+}
